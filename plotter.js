c0f2c471cc0a1e6af590287df1d6ebcb1f171164
Rob Christian <rm.rf.etc@gmail.com>
2016-11-22 02:41:31 -0800

    Add horizontal scaling

--- plotter.js
+++ plotter.js
@@ -1,13 +1,16 @@
 
-var paper1 = Snap(1000, 400)
-var paper2 = Snap(1000, 400)
+var paper1 = Snap(800, 400)
+var paper2 = Snap(800, 400)
 paper1.attr({ class: 'line1' })
 paper2.attr({ class: 'line2' })
 
 
 // constants
-var a = 0.01025
-var l = 20
+var res = 10
+var len = 50
+var a, i
+a = (4.0325 / (len*len))
+// a = (0.0205 / (len*len))
 
 // globals
 var v = 0
@@ -15,7 +18,7 @@ var p = 0
 
 
 function quadratic (x) {
-	var x = x / (l * 0.5)
+	var x = x / (len * 0.5)
 	var res = x * x
 	return res
 }
@@ -24,9 +27,7 @@ function data1 (res) {
 
 	var i = 0
 	var array = []
-	while (i<res) {
-		array[array.length] = i++
-	}
+	while (i<res) array[array.length] = i++
 
 	return array
 }
@@ -36,13 +37,13 @@ function draw1 () {
 	console.log("Draw 1")
 	var points = []
 
-	data1(10).forEach(function(idx){
+	data1(15).forEach(function(idx){
 
-		var x = idx * 50
-		var y = parseFloat( (quadratic(idx)*200).toPrecision(6) )
+		var x = idx
+		var y = parseFloat( (quadratic(idx)).toPrecision(6) )
 
 		console.log( x, y )
-		points.push( x, y )
+		points.push( x * 25, y * 1250 )
 	})
 
 	var p1 = paper1.polyline(points)
@@ -52,16 +53,19 @@ function draw1 () {
 
 function kinematic (td, time) {
 
-	var dir = (time < l / 2) ? 1 : -1
+	var dir = (time < len / 2) ? 1 : -1
 	v = parseFloat((v + dir * a * td).toPrecision(8))
 	p = parseFloat((p + v * td).toPrecision(8))
 
 	return p
 }
 
-function data2 (res, val) {
+function data2 (res, td) {
 
-	return Array(20 * res).fill(val/res || 1/res, 0, 20 * res)
+	if (!res) return []
+
+	// var val = (td) ? (td/res) : (1/res)
+	return Array(len*res).fill( 1/res, 0, len*res )
 }
 
 function draw2 () {
@@ -71,13 +75,13 @@ function draw2 () {
 	var time = new Timer()
 	var points = []
 
-	data2(8, 1).forEach(function(idx){
+	data2(res).forEach(function(idx){
 
 		var x = time.tick(idx)
 		var y = parseFloat( kinematic(idx, time.get()).toPrecision(6) )
 
 		console.log( x, y )
-		points.push( x * 50, y * 400 )
+		points.push( x * 10, y * 400 )
 	})
 
 	var p2 = paper2.polyline(points)
