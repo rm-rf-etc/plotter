5695172a2ca7b629531bd42925ed89bde6759715
Robert Christian <rchristian@truecar.com>
2016-12-14 11:28:53 -0800

    Add piecewise s-curve generator.

--- plotter.js
+++ plotter.js
@@ -14,7 +14,7 @@ var _res = 4
 var _res1 = 3 // red
 var _res2 = 5 // blue
 var _res3 = _res * 5 // green
-var _len = 800
+var _len = 400
 var _amp = 400
 var _acc = 0.5
 var repeats = 1
@@ -22,20 +22,20 @@ var imprecise = false
 
 var sim1 = new KinematicCurve({
 	name: 'red A ' + _res1,
-	len: _len,
-	amp: _amp,
+	len: 1,
+	amp: 1,
 	acc: _acc,
 })
 var sim2 = new KinematicCurve({
 	name: 'blue B ' + _res2,
-	len: _len,
-	amp: _amp,
+	len: 1,
+	amp: 1,
 	acc: _acc,
 })
 var sim3 = new KinematicCurve({
 	name: 'green C ' + _res3,
-	len: _len,
-	amp: _amp,
+	len: 1,
+	amp: 1,
 	acc: _acc,
 })
 
@@ -78,33 +78,52 @@ chart1.polyline(bottomMarker2).attr({stroke:col2})
 
 
 var plot3 = new PlotLine(chart1, { stroke:colC })
-plot3.draw( generateCurve(_res3, _len, sim3) )
+plot3.draw( runCurveSim(_res3, _len, sim3.tick) )
 
 var plot2 = new PlotLine(chart1, { stroke:colB })
-plot2.draw( generateCurve(_res2, _len, sim2) )
+plot2.draw( runCurveSim(_res2, _len, sim2.tick) )
 
 var plot1 = new PlotLine(chart1, { stroke:colA })
-plot1.draw( generateCurve(_res1, _len, sim1) )
+plot1.draw( runCurveSim(_res1, _len, sim1.tick) )
+
+var plot1 = new PlotLine(chart1, { stroke:colA })
+var plot2 = new PlotLine(chart1, { stroke:colB })
+
+var f = simpleCurve1(2/5)
+plot1.draw( runCurveSim(40, 1, function(x){
+
+	var y = f(x)
+	console.log({
+		x: +x.toPrecision(3),
+		y: +y.toPrecision(3),
+	})
+
+	return y
+}, true))
+
+// plot2.draw( runCurveSim(40, 1, function(x){
+// 	return (3 - 2 * x) * x * x
+// }))
 
 
-function generateCurve (res, len, sim) {
+function runCurveSim (res, len, sim, dtoggle) {
 
 	var x = 0
 	var y = 0
+	var t = 0
 	var timeInterval = len/res
-	var timeSteps = ([0,0]).concat(Array(res).fill(timeInterval, 0, res))
+	var timeSteps = ([0]).concat(Array(res).fill(timeInterval, 0, res))
 
 	return timeSteps.map(function (idx) {
 
 		if (imprecise) {
 			idx += Math.random() * 4 - 2
 		}
-		x += idx
-		y = parseFloat( sim.tick(idx).toPrecision(10) )
-		return [x, y]
+		t += idx
+		y = parseFloat( sim(dtoggle ? t : idx).toPrecision(10) )
+		return [t*_len, y*_amp]
 	})
 }
-
 
 
 
@@ -207,4 +226,51 @@ function KinematicCurve (opts) {
 			"actual": parseFloat(pos.toPrecision(8)),
 		})
 	}
+}
+
+
+function simpleCurve1 (pcntMid, pcntLow, _a, _b, _c, _f, p1, p2) {
+
+	tick.updateShape = updateShape
+	tick.maxVelocity = maxVelocity
+
+	updateShape(pcntMid)
+
+	function tick (x) {
+
+		switch (true) {
+
+			case (x >= 1):
+				return 1
+
+			case (x >= 0 && x < p1):
+				return 1 / _b * Math.pow(x * _a, 2)
+
+			case (x >= p1 && x < p2):
+				return 0.5 + _c * (x - 0.5)
+
+			case (x >= p2 && x <= 1):
+				return 1 - (_a * _a) / _b * Math.pow(1 - x, 2)
+		}
+	}
+
+	function updateShape (pcntMid) {
+
+		pcntMid = Math.max(0, Math.min(1, pcntMid))
+		pcntLow = (1 - pcntMid) * 0.5
+		p1 = pcntLow
+		p2 = pcntLow + pcntMid
+
+		_f = (pcntLow == 0) ? 1 : pcntLow
+		_a = 1 / _f
+		_b = (_a - 2) * 2 + 2
+		_c = (_f == 1) ? _a : _a / (_b * 0.5)
+	}
+
+	function maxVelocity () {
+
+		return _c
+	}
+
+	return tick
 }
